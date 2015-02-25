26b4671c1a5a30da2e9d6787ef30e93e38e099c1
Hao SUN <hasun@zendesk.com>
2015-02-25 15:12:27 -0800

    Initial Commit

---
+++ Animaticons/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Animaticons
+//
+//  Created by Hao Sun on 2/25/15.
+//  Copyright (c) 2015 Hao Sun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
